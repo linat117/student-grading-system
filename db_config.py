67046c0e8d71b014f3fadd83d17f7280862e59ed
Hilina Teshome <teshilina8@gmail.com>
2025-07-08 02:09:09 +0300

    Ready to be deployed

--- db_config.py
+++ db_config.py
@@ -1,10 +1,12 @@
+import os
 import mysql.connector
+from flask import Flask
 
-def db_connection():
-    
-       return mysql.connector.connect(
-            host = "localhost",
-            user="root",
-            password = "",
-            database = "student-grading-system"
-        )
+app = Flask(__name__)
+
+db_connection = mysql.connector.connect(
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME")
+)
