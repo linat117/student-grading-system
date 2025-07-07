2a2ee056087fffdc41d5a9c17c8d5f052fe489a7
Hilina Teshome <teshilina8@gmail.com>
2025-07-08 00:54:59 +0300

    Student logic created

--- backend/student.py
+++ backend/student.py
@@ -1,8 +1,40 @@
 from db_config import db_connection 
 
 def add_student():
-    student_name = input("Enter student name")
-    pass
+    try:
+        db = db.connection()
+        cursor = db.cursor()
 
+        student_id = int(input("Enter studentID: "))
+        name = input("Enter student name: ")
+        age = int(input("Enter age: "))
+        major = input("Enter major: ")
+
+        cursor.execute("""
+            INSERT INTO students (id, name, age, major)
+            VALUES (%s, %s, %s, %s)
+        """, (student_id, name, age, major))
+
+        db.commit()
+        print("✅ Student added successfully.")
+
+    except Exception as e:
+        print("❌ Error adding student:", e)
+    finally:
+        cursor.close()
+        db.close()
 def list_students():
-    pass
+     try:
+        db = db_connection()
+        cursor = db.cursor()
+
+        cursor.execute("SELECT * FROM students")
+        students = cursor.fetchall()
+        for student in students:
+            print(f"ID: {student[0]} | Name: {student[1]} | Age: {student[2]} | Major: {student[3]}")
+
+     except Exception as e:
+        print("❌ Error fetching students:", e)
+     finally:
+        cursor.close()
+        db.close()
